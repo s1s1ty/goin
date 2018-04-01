dbcd047fe7485eef8cf4f7a1e48266f95fb328f8
shaonty <shaonty@pathao.com>
2018-04-01 16:59:46 +0600

    interface added and signature updated

--- in.go
+++ in.go
@@ -1,21 +1,12 @@
 package goin
 
 import (
+	"log"
 	"sort"
 )
 
-// Interface is describing `goin` features
-type Interface interface {
-	IntIn(ar []int, value int) bool
-	// Int64In(ar []int64, value int64) bool # need to implement
-	// FloatIn(ar []int64, value int64) bool # need to implement
-	// InterfaceIn(ar []int64, value int64) bool # need to implement
-	Float64In(ar []float64, value float64) bool
-	StringIn(ar []string, value string) bool
-}
-
-// IntIn returns boolean value based on int sequence contains value variable or not
-func IntIn(ar []int, value int) bool {
+// InInt returns boolean value based on int sequence contains value variable or not
+func InInt(ar []int, value int) bool {
 	sort.Ints(ar)
 	start := 0
 	end := len(ar) - 1
@@ -32,8 +23,8 @@ func IntIn(ar []int, value int) bool {
 	return false
 }
 
-// Float64In returns boolean value based on Float64 sequence contains value variable or not
-func Float64In(ar []float64, value float64) bool {
+// InFloat64 returns boolean value based on Float64 sequence contains value variable or not
+func InFloat64(ar []float64, value float64) bool {
 	sort.Float64s(ar)
 	start := 0
 	end := len(ar) - 1
@@ -50,8 +41,8 @@ func Float64In(ar []float64, value float64) bool {
 	return false
 }
 
-// StringIn returns boolean value based on string sequence contains value variable or not
-func StringIn(ar []string, value string) bool {
+// InString returns boolean value based on string sequence contains value variable or not
+func InString(ar []string, value string) bool {
 	sort.Strings(ar)
 	start := 0
 	end := len(ar) - 1
@@ -64,6 +55,45 @@ func StringIn(ar []string, value string) bool {
 		} else if ar[mid] > value {
 			end = mid - 1
 		}
+	}
+	return false
+}
+
+// In returns boolean based interface
+func In(ar interface{}, value interface{}, typ string) bool {
+	switch typ {
+	case "int":
+		seq, ok := ar.([]int)
+		if ok != true {
+			log.Fatal("Convert typ is `int` but sequence type is not `int`")
+		}
+		val, ok := value.(int)
+		if ok != true {
+			log.Fatal("Convert typ is `int` but value type is not `int`")
+		}
+		return InInt(seq, val)
+	case "float64":
+		seq, ok := ar.([]float64)
+		if ok != true {
+			log.Fatal("Convert typ is `float64` but sequence type is not `float64`")
+		}
+		val, ok := value.(float64)
+		if ok != true {
+			log.Fatal("Convert typ is `float64` but value type is not `float64`")
+		}
+		return InFloat64(seq, val)
+	case "string":
+		seq, ok := ar.([]string)
+		if ok != true {
+			log.Fatal("Convert typ is `string` but sequence type is not `string`")
+		}
+		val, ok := value.(string)
+		if ok != true {
+			log.Fatal("Convert typ is `string` but value type is not `string`")
+		}
+		return InString(seq, val)
+	default:
+		log.Fatal("Unknown type")
 	}
 	return false
 }
--- in_test.go
+++ in_test.go
@@ -2,39 +2,47 @@ package goin
 
 import "testing"
 
-func Test_IntIn(t *testing.T) {
+func Test_InInt(t *testing.T) {
 	ar := []int{1, 4, 5, 2, 90}
 	value := 5
-	test1 := IntIn(ar, value)
+	test1 := InInt(ar, value)
 	if test1 != true {
-		t.Error("IntIn should return true but get", test1)
+		t.Error("InInt should return true but get", test1)
 	}
-	test2 := IntIn(ar, 8)
+	test2 := InInt(ar, 8)
 	if test2 != false {
-		t.Error("IntIn should return true but get", test2)
+		t.Error("InInt should return true but get", test2)
 	}
 }
-func Test_Float64In(t *testing.T) {
+func Test_InFloat64(t *testing.T) {
 	ar := []float64{1.2, 4.110, 5.98, 2.78, 9.0}
 	value := 5.9
-	test1 := Float64In(ar, value)
+	test1 := InFloat64(ar, value)
 	if test1 != false {
-		t.Error("IntIn should return true but get", test1)
+		t.Error("InFloat64 should return true but get", test1)
 	}
-	test2 := Float64In(ar, 4.11)
+	test2 := InFloat64(ar, 4.11)
 	if test2 != true {
-		t.Error("IntIn should return true but get", test2)
+		t.Error("InFloat64 should return true but get", test2)
 	}
 }
-func Test_StringIn(t *testing.T) {
+func Test_InString(t *testing.T) {
 	ar := []string{"apple", "orange", "pine", "lemon"}
 	value := "appl"
-	test1 := StringIn(ar, value)
+	test1 := InString(ar, value)
 	if test1 != false {
-		t.Error("IntIn should return true but get", test1)
+		t.Error("InString should return true but get", test1)
 	}
-	test2 := StringIn(ar, "orange")
+	test2 := InString(ar, "orange")
 	if test2 != true {
-		t.Error("IntIn should return true but get", test2)
+		t.Error("InString should return true but get", test2)
+	}
+}
+
+func Test_In(t *testing.T) {
+	ar := []string{"apple", "orange", "pine", "lemon"}
+	test1 := In(ar, "apple", "string")
+	if test1 != true {
+		t.Error("In should return true but get ", test1)
 	}
 }
